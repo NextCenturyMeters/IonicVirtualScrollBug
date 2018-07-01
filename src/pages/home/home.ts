d05ac0955f2d9b31466458d85e8db1c31227b2cc
Adam <adam.paul88@gmail.com>
2018-07-01 13:16:44 -0600

    initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+export interface ITestListData {
+  _id: number;
+  name: string;
+  value: number;
+}
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public testListData: ITestListData[] = [];
+  public selectedItem: number = null;
 
+  constructor(public navCtrl: NavController) {
+    for(let i = 0; i < 1000; i++) {
+      this.testListData.push({
+        _id: i,
+        name: 'Item #' + i,
+        value: i,
+      });
+    }
+  }
+
+  public logClick(id) {
+    console.log('Item Clicked:', id, 'Selected Item:', this.selectedItem);
   }
 
 }
